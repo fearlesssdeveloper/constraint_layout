661b4a00243cb93a5e394397dd35034f68f415f1
Arlan Linux <marinhomoraisster@gmail.com>
2021-03-29 22:29:44 -0300

    ConstraintLayout_1 - 01 - Criando o projeto

---
+++ app/src/main/java/br/com/alura/aluraviagens/ListaPacotesActivity.kt
@@ -0,0 +1,11 @@
+package br.com.alura.aluraviagens
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListaPacotesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lista_pacotes)
+    }
+}
