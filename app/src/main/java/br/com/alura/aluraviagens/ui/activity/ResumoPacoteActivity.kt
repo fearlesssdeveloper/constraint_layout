23b1505bdd187fd6416b0c03989061acdb648ef9
Arlan Linux <marinhomoraisster@gmail.com>
2021-04-01 11:09:39 -0300

    constraint_layout_2 - 01 - Criando a tela de resumo do pacote

---
+++ app/src/main/java/br/com/alura/aluraviagens/ui/activity/ResumoPacoteActivity.kt
@@ -0,0 +1,12 @@
+package br.com.alura.aluraviagens.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.alura.aluraviagens.R
+
+class ResumoPacoteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_resumo_pacote)
+    }
+}
