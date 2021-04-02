f7129328e7075f790c05fd1062f0c717173304b5
Arlan Linux <marinhomoraisster@gmail.com>
2021-04-02 00:03:07 -0300

    constraint_layout_2 - 02 - Adicionando a data da viagem e título

--- app/src/main/java/br/com/alura/aluraviagens/ui/activity/ResumoPacoteActivity.kt
+++ app/src/main/java/br/com/alura/aluraviagens/ui/activity/ResumoPacoteActivity.kt
@@ -9,16 +9,27 @@ import br.com.alura.aluraviagens.extensions.formataParaBrasileiro
 import br.com.alura.aluraviagens.model.Pacote
 import kotlinx.android.synthetic.main.activity_resumo_pacote.*
 import java.math.BigDecimal
+import java.text.SimpleDateFormat
+import java.util.*
 
 class ResumoPacoteActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_resumo_pacote)
 
+        title = "Resumo do pacote"
         val pacoteSaoPaulo = Pacote("São Paulo", "sao_paulo_sp", 2, BigDecimal("244.99"))
         resumo_pacote_local.text = pacoteSaoPaulo.local
         resumo_pacote_imagem.setImageDrawable(pacoteSaoPaulo.imagem.devolveDrawable(this))
         resumo_pacote_dias.text = pacoteSaoPaulo.dias.formataDiasParaTexto()
         resumo_pacote_preco.text = pacoteSaoPaulo.preco.formataParaBrasileiro()
+        val dataIda = Calendar.getInstance()
+        val dataVolta = Calendar.getInstance()
+        dataVolta.add(Calendar.DATE, pacoteSaoPaulo.dias)
+        val formatoBrasileiro = SimpleDateFormat("dd/MM")
+        val dataFormatadaIda = formatoBrasileiro.format(dataIda.time)
+        val dataFormatadaVolta = formatoBrasileiro.format(dataVolta.time)
+        val dataFormatadaDaViagem = "$dataFormatadaIda - $dataFormatadaVolta de ${dataFormatadaVolta[Calendar.YEAR]}"
+        resumo_pacote_data.text = dataFormatadaDaViagem
     }
 }
