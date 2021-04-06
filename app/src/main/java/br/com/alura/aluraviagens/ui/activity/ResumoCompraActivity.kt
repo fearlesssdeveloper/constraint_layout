13901df61b4dde738593d557d99f04b08d1e8287
Arlan Linux <marinhomoraisster@gmail.com>
2021-04-06 12:41:08 -0300

    constraint_layout_2 - 04 - Realizando o bind

--- app/src/main/java/br/com/alura/aluraviagens/ui/activity/ResumoCompraActivity.kt
+++ app/src/main/java/br/com/alura/aluraviagens/ui/activity/ResumoCompraActivity.kt
@@ -3,10 +3,24 @@ package br.com.alura.aluraviagens.ui.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import br.com.alura.aluraviagens.R
+import br.com.alura.aluraviagens.extensions.devolveDrawable
+import br.com.alura.aluraviagens.extensions.formataDiasParaTexto
+import br.com.alura.aluraviagens.extensions.formataParaBrasileiro
+import br.com.alura.aluraviagens.model.Pacote
+import kotlinx.android.synthetic.main.activity_resumo_compra.*
+import java.math.BigDecimal
 
 class ResumoCompraActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_resumo_compra)
+
+        title = "Resumo da compra"
+
+        val pacoteSaoPaulo = Pacote("São Paulo", "sao_paulo_sp", 2, BigDecimal("244.99"))
+        activity_resumo_compra_local_pacote.text = pacoteSaoPaulo.local
+        activity_resumo_compra_imagem_pacote.setImageDrawable(pacoteSaoPaulo.imagem.devolveDrawable(this))
+        activity_resumo_compra_data_viagem.text = pacoteSaoPaulo.dias.formataDiasParaTexto()
+        activity_resumo_compra_preco_pacote.text = pacoteSaoPaulo.preco.formataParaBrasileiro()
     }
 }
